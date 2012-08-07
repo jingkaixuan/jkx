9e75ca7f862758b3fb75e2da0b10c8689a5b55ca
jingkaixuan <jkaixuan@gmail.com>
2012-08-07 20:11:47 +0800

    继续分析globe，但存在一个错误，计算射线的时候DrawContext是null（BasicView Line：1006）

--- jkx/test/net/jkx/test/worldwind/abc/GlobeAnalysis.java
+++ jkx/test/net/jkx/test/worldwind/abc/GlobeAnalysis.java
@@ -1,11 +1,15 @@
 package net.jkx.test.worldwind.abc;
 
 import gov.nasa.worldwind.Model;
+import gov.nasa.worldwind.View;
 import gov.nasa.worldwind.WorldWind;
 import gov.nasa.worldwind.WorldWindow;
 import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.Extent;
+import gov.nasa.worldwind.geom.Intersection;
+import gov.nasa.worldwind.geom.Line;
 import gov.nasa.worldwind.globes.Globe;
 
 import java.awt.BorderLayout;
@@ -27,15 +31,33 @@ public class GlobeAnalysis {
 				.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
 		wwd.setModel(m);
 
+		Globe globe = m.getGlobe();
 		// Extent
 		{
-			Globe globe = m.getGlobe();
-			Extent extent = globe.getExtent();
-			System.out.println("Extent class: " + extent.getClass()); 
+			Extent extent = globe.getExtent(); // Earth.getExtent() return itself.
+			System.out.println("(1)Extent class: " + extent.getClass()); 
 			System.out.println("    center: " + extent.getCenter()); // center: (0.0, 0.0, 0.0, 1.0)
 			System.out.println("    diameter: " + extent.getDiameter());// diameter: 1.2756274E7
 			System.out.println("    radius: " + extent.getRadius()); // radius: 6378137.0
 		}
+		
+		System.out.println("(2)globe's equatorial(赤道) Radius: " + globe.getEquatorialRadius());
+		System.out.println("(3)globe's polar(两极) radius: " + globe.getPolarRadius());
+		System.out.println("(4)globe's radius at (110°E, 35°N): " + globe.getRadiusAt(Angle.fromDegrees(35), Angle.fromDegrees(110)));
+		System.out.println("(5)globe's radius at (110°E, 0°): " + globe.getRadiusAt(Angle.fromDegrees(0), Angle.fromDegrees(110)));
+		System.out.println("(6)elevation of Everest(珠峰海拔，数据不准确): " + globe.getElevation(Angle.fromDMS(27, 59, 14), Angle.fromDMS(86, 55, 26)) + " m");
+		System.out.println("(7)偏心的平方（不知道是什么东东）: " + globe.getEccentricitySquared());
+		
+		// intersect
+		{
+			View view = wwd.getView();
+			Line ray = view.computeRayFromScreenPoint(80, 100);
+//			double eyeElevation = view.getEyePosition().getElevation();
+			Intersection[] intersections = globe.intersect(ray);
+			System.out.println(intersections.length);
+		}
+		
+		
 
 		// 创建一个JFrame用于显示wwd
 		final JFrame frame = new JFrame();
